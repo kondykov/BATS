0432f265770b7fa9588c80ec3e031b043e62564e
kondykov <kondykovs@gmail.com>
2024-12-16 12:45:33 +0300

    Привязка свойства Статус

--- BATS/ViewModels/MainWindowViewModel.cs
+++ BATS/ViewModels/MainWindowViewModel.cs
@@ -5,14 +5,32 @@ namespace BATS.ViewModels;
 internal class MainWindowViewModel : ViewModel
 {
     #region Title
+
     private string? _title = "Анализ статистики";
+
     /// <summary>
-    /// Заголовок окна
+    ///     Заголовок окна
     /// </summary>
     public string? Title
     {
         get => _title;
         set => SetProperty(ref _title, value);
     }
+
+    #endregion
+
+    #region Status : string - Статус программы
+
+    private string? _status = "Готов!";
+
+    /// <summary>
+    ///     Статус программы
+    /// </summary>
+    public string? Status
+    {
+        get => _status;
+        set => SetProperty(ref _status, value);
+    }
+
     #endregion
 }
