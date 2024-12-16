9da7db5687ef39e6623186a182b16101c71941c9
kondykov <kondykovs@gmail.com>
2024-12-16 11:40:50 +0300

    Тестирование работы модели представления и главного окна

---
+++ BATS/ViewModels/MainWindowViewModel.cs
@@ -0,0 +1,18 @@
+﻿using BATS.ViewModels.Base;
+
+namespace BATS.ViewModels;
+
+internal class MainWindowViewModel : ViewModel
+{
+    #region Title
+    private string? _title = "Анализ статистики";
+    /// <summary>
+    /// Заголовок окна
+    /// </summary>
+    public string? Title
+    {
+        get => _title;
+        set => SetProperty(ref _title, value);
+    }
+    #endregion
+}
