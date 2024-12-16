18407625a9382acda454bab0f4b00cc395a29aa8
kondykov <kondykovs@gmail.com>
2024-12-16 12:58:16 +0300

    Базовый класс команды

---
+++ BATS/Infrastructure/Commands/Base/Command.cs
@@ -0,0 +1,16 @@
+﻿using System.Windows.Input;
+
+namespace BATS.Infrastructure.Commands.Base;
+
+internal abstract class Command : ICommand
+{
+    public abstract bool CanExecute(object? parameter);
+
+    public abstract void Execute(object? parameter);
+
+    public event EventHandler? CanExecuteChanged
+    {
+        add { CommandManager.RequerySuggested += value; }
+        remove { CommandManager.RequerySuggested -= value; }
+    }
+}
