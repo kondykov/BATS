bcdcbe89dae9e5663e66253a138d7b800003272f
kondykov <kondykovs@gmail.com>
2024-12-16 23:17:37 +0300

    Fetching .csv

--- BATSConsole/Program.cs
+++ BATSConsole/Program.cs
@@ -1,9 +1,15 @@
 ﻿namespace BATSConsole;
 
-class Program
+internal static class Program
 {
+    private const string CvsUrl = @"https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_time_series/time_series_covid19_confirmed_global.csv";
     static void Main(string[] args)
     {
+        var client = new HttpClient();
+        
+        var response = client.GetAsync(CvsUrl).Result;
+        var content = response.Content.ReadAsStringAsync().Result;
+        
         Console.WriteLine("Hello, World!");
     }
 }
