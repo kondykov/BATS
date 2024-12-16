9f9934eb6038a798493d818f36e88ea5a6a638ea
kondykov <kondykovs@gmail.com>
2024-12-17 00:09:32 +0300

    Извлечение данных по датам из записей

--- BATSConsole/Program.cs
+++ BATSConsole/Program.cs
@@ -1,15 +1,47 @@
-﻿namespace BATSConsole;
+﻿using System.Globalization;
+
+namespace BATSConsole;
 
 internal static class Program
 {
-    private const string CvsUrl = @"https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_time_series/time_series_covid19_confirmed_global.csv";
-    static void Main(string[] args)
+    private const string CvsUrl =
+        @"https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_time_series/time_series_covid19_confirmed_global.csv";
+
+    private static async Task<Stream> GetDataStream()
     {
         var client = new HttpClient();
+        var response = await client.GetAsync(CvsUrl, HttpCompletionOption.ResponseHeadersRead);
+        return await response.Content.ReadAsStreamAsync();
+    }
+
+    public static IEnumerable<string> ReadLines()
+    {
+        using var dataStream = GetDataStream().Result;
+        using var streamReader = new StreamReader(dataStream);
+
+        while (!streamReader.EndOfStream)
+        {
+            var line = streamReader.ReadLineAsync().Result;
+            yield return line;
+        }
+    }
+
+    private static DateTime[] GetDates()
+    {
+        return ReadLines()
+            .First()
+            .Split(',')
+            .Skip(4)
+            .Select(s => DateTime.Parse(s, CultureInfo.InvariantCulture)).ToArray();
+    }
+    
+
+    private static void Main(string[] args)
+    {
+        var dates = GetDates();
         
-        var response = client.GetAsync(CvsUrl).Result;
-        var content = response.Content.ReadAsStringAsync().Result;
+        Console.WriteLine(string.Join("\r\n", dates));
         
-        Console.WriteLine("Hello, World!");
+        // foreach (var dataLine in ReadLines()) Console.WriteLine(dataLine);
     }
 }
